184dc670c51f56a77c70acc60a948a600dcff1ad
Megan <megansvasconcelos@gmail.com>
2018-07-30 13:43:32 -0400

    second commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-       Do or do not, there is no try
+       These aren't the droids you're lookign for
       </div>
     );
   }
