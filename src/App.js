463da38a266ff781bedfe3fb1b1b074388245a81
Megan <megansvasconcelos@gmail.com>
2018-07-30 13:46:55 -0400

    third commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-       These aren't the droids you're lookign for
+       These aren't the droids you're looking for.
       </div>
     );
   }
