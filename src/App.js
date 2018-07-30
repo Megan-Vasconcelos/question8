cd8dd64f921010a7d77ce7de6578b9ef2cf04fbf
Megan <megansvasconcelos@gmail.com>
2018-07-30 13:26:19 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       Do or do not, there is no try
+      </div>
+    );
+  }
+}
+
+export default App;
